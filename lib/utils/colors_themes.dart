005f08ce5d488afca7520f42ae51b7dcab3639f6
sajid shaikh <sajid20shaikh@gmail.com>
2022-08-15 15:42:44 +0530

    adding constant utils and colors

---
+++ lib/utils/colors_themes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const Color yellowColor = Color(0xfffed813);
+
+const Color activeCyancolor = Color(0xff0a7c97);
+
+const Color backgroundColor = Color(0xffebecee);
+
+const List<Color> backgroundGradient = [Color(0xff80d9e9), Color(0xffa0e9ce)];
+// cyan, and a mix of cyan and green
+
+const List<Color> lightBackgroundGradient = [
+  Color(0xffa2e0eb),
+  Color.fromARGB(255, 200, 228, 218)
+];
