6502434c206cac071c6c251a870940ec5f1cdf48
sajid shaikh <sajid20shaikh@gmail.com>
2022-08-16 00:46:20 +0530

    fire base connetivity

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:amazon_clonee/screens/sign_in_screens.dart';
 import 'package:amazon_clonee/utils/colors_themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyAGlMfVLbUL8RqUdNXXIPS95SyMHhRTs2o",
+            authDomain: "clone-1d9e2.firebaseapp.com",
+            projectId: "clone-1d9e2",
+            storageBucket: "clone-1d9e2.appspot.com",
+            messagingSenderId: "780068311587",
+            appId: "1:780068311587:web:6fb11ed732dd62518d5796"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
